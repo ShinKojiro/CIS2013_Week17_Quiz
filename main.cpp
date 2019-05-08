6588ab3b138039f176047d77692a2170aa53ab5c
ShinKojiro <42621442+ShinKojiro@users.noreply.github.com>
2019-05-08 18:49:33 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,19 +18,34 @@ int main(){
     
     double litersConsumed;
     double milesTraveled;
+    char cont = 'a';
     
-
     cout << "This is a liters per gallon to miles per gallon converter," << endl;
     cout << "for those weirdos who read their fuel capacity in liters!" << endl;
 
-    cout << "Enter the number of liters you used today: ";
-    cin >> litersConsumed;
-    cout << endl << "Enter the number of miles you drove: ";
-    cin >> milesTraveled;
+    while(cont != 'n' && cont != 'N'){
+        
+        cout << "Enter the number of liters you used today: ";
+        cin >> litersConsumed;
+        cout << endl << "Enter the number of miles you drove: ";
+        cin >> milesTraveled;
 
-    litersPerMile = litersConsumed / milesTraveled;
-    literToGallon(litersPerMile);
+        litersPerMile = litersConsumed / milesTraveled;
+        literToGallon(litersPerMile);
     
-    cout << endl << endl << "you used " << litersPerMile << " liters per mile." << endl;
-    cout << "In gallons per mile, that will be " << gallonsPerMile << " gallons per mile" << endl;
+        cout << endl << endl << "you used " << litersPerMile << " liters per mile." << endl;
+        cout << "In gallons per mile, that will be " << gallonsPerMile << " gallons per mile" << endl;
+        cout << endl << "Do you want to enter new values? (y/n)";
+        cin >> cont;
+        switch(cont){
+            case 'n':
+                cout << "Thanks for using, remember we use gallons round 'er !";
+                break;
+            default:
+                break;
+        }
+    }
+    
+
+    
 }
